4830d5d530557371a352a8723e7ec042211a15d7
rodrigoxy <rodrigo.xy.silva@scania.com>
2025-03-11 16:10:13 -0300

    codigo-completo

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//armazena os nomes que já sairam
+let nomesSorteados = [];
+
 //função adicionar amigos
 function adicionarAmigo() {
     //recebendo valores do input amigo
@@ -9,7 +12,7 @@ function adicionarAmigo() {
         let lista = document.getElementById("listaAmigos"); // Pegando a lista
         let item = document.createElement("li"); // Criando um item de lista
         item.textContent = textoInput;
-        lista.appendChild(item); // Adicionando o item à lista
+        lista.appendChild(item); // Adicionando nomes à lista
 
         inputAmigo.value = ""; // Limpando o input
     }
@@ -23,13 +26,36 @@ document.getElementById("amigo").addEventListener("keypress", function(event) {
     }
 });
 
-//função adicionar amigos
-function adicionarAmigo(){
-    let inputAmigo = document.getElementById("amigo")
-    let textoInput = inputAmigo.value
-    console.log(textoInput)
-}
-
 function sortearAmigo(){
+    let lista = document.getElementById("listaAmigos");
+    let items = lista.getElementsByTagName("li");
 
+    if (items.length < 2) {
+        alert("Adicione pelo menos 2 amigos");
+        console.log(items.length);
+    } else if (nomesSorteados.length === items.length) {
+        alert("Todos os nomes foram selecionados");
+        console.log("Todos os nomes foram selecionados");
+    } else {
+        let amigoSorteado;
+        do {
+            // Seleciona um índice aleatório da lista de amigos
+            let indiceAleatorio = Math.floor(Math.random() * items.length);
+            amigoSorteado = items[indiceAleatorio].textContent;
+        } while (nomesSorteados.includes(amigoSorteado));
+
+        // Adiciona o nome sorteado ao array de nomes sorteados
+        nomesSorteados.push(amigoSorteado);
+
+        // Exibe o nome do amigo sorteado
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `<li style="color: green;">Amigo sorteado: ${amigoSorteado}</li>`;
+
+        console.log("Amigo sorteado: " + amigoSorteado);
+        
+        // Limpa o resultado após 2 segundos
+        setTimeout(() => {
+            resultado.innerHTML = "";
+        }, 2000);
+    }
 }
